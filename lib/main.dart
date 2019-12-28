49e93626f3fb8f03d9dc0432c68826298072bf26
Mohamed Ahmed <error17191@gmail.com>
2019-12-28 14:10:56 +0200

    Decreased distances between piecies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 List<PuzzlePiece> puzzlePieces = List<PuzzlePiece>();
 List<_PuzzlePieceState> pieceStates = List<_PuzzlePieceState>();
 double inBetweenDistance;
 double sideMargin;
-double pieceDimention;
+double imageWidth;
+double imageHeight = 100.0;
+var images = [
+  {'order': 0, 'path': 'assets/images/1.jpg'},
+  {'order': 1, 'path': 'assets/images/2.jpg'},
+  {'order': 2, 'path': 'assets/images/3.jpg'},
+  {'order': 3, 'path': 'assets/images/4.jpg'},
+  {'order': 4, 'path': 'assets/images/5.jpg'},
+  {'order': 5, 'path': 'assets/images/6.jpg'},
+  {'order': 6, 'path': 'assets/images/7.jpg'},
+  {'order': 7, 'path': 'assets/images/8.jpg'},
+  {'order': 8, 'path': 'assets/images/9.jpg'},
+];
 
 void main() {
   runApp(MyApp());
@@ -19,8 +34,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class AnyThing {}
-
 class DragApp extends StatefulWidget {
   @override
   _DragAppState createState() => _DragAppState();
@@ -29,72 +42,65 @@ class DragApp extends StatefulWidget {
 class _DragAppState extends State<DragApp> {
   @override
   Widget build(BuildContext context) {
-    pieceDimention = MediaQuery.of(context).size.width * 0.3;
-    sideMargin = MediaQuery.of(context).size.width * 0.03;
-    inBetweenDistance = MediaQuery.of(context).size.width * 0.02;
+    images.shuffle();
 
-    puzzlePieces.add(PuzzlePiece(0, 'assets/images/1.jpg', 100.0, sideMargin));
-    puzzlePieces.add(PuzzlePiece(1, 'assets/images/2.jpg', 100.0,
-        sideMargin + pieceDimention + inBetweenDistance));
-    puzzlePieces.add(PuzzlePiece(2, 'assets/images/3.jpg', 100.0,
-        sideMargin + pieceDimention * 2 + inBetweenDistance * 2));
-    puzzlePieces.add(PuzzlePiece(3, 'assets/images/4.jpg', 200.0, sideMargin));
-    puzzlePieces.add(PuzzlePiece(4, 'assets/images/5.jpg', 200.0,
-        sideMargin + pieceDimention + inBetweenDistance));
-    puzzlePieces.add(PuzzlePiece(5, 'assets/images/6.jpg', 200.0,
-        sideMargin + pieceDimention * 2 + inBetweenDistance * 2));
-    puzzlePieces.add(PuzzlePiece(6, 'assets/images/7.jpg', 300.0, sideMargin));
-    puzzlePieces.add(PuzzlePiece(7, 'assets/images/8.jpg', 300.0,
-        sideMargin + pieceDimention + inBetweenDistance));
-    puzzlePieces.add(PuzzlePiece(8, 'assets/images/9.jpg', 300.0,
-        sideMargin + pieceDimention * 2 + inBetweenDistance * 2));
+    inBetweenDistance = 2;
+    double widthWithoutMargins =
+        MediaQuery.of(context).size.width - 2 * inBetweenDistance;
+    imageWidth = widthWithoutMargins * 0.32;
+    sideMargin = widthWithoutMargins * 0.02;
 
-    return Stack(
-      children: [
-        puzzlePieces[0],
-        puzzlePieces[1],
-        puzzlePieces[2],
-        puzzlePieces[3],
-        puzzlePieces[4],
-        puzzlePieces[5],
-        puzzlePieces[6],
-        puzzlePieces[7],
-        puzzlePieces[8],
-      ],
+    return FutureBuilder<Size>(
+      future: _calculateImageDimension(),
+      // a previously-obtained Future<String> or null
+      builder: (BuildContext context, AsyncSnapshot<Size> snapshot) {
+        if (snapshot.hasData) {
+          imageHeight = snapshot.data.height * imageWidth / snapshot.data.width;
+          return Stack(
+            children: [
+              PuzzlePiece(0),
+              PuzzlePiece(1),
+              PuzzlePiece(2),
+              PuzzlePiece(3),
+              PuzzlePiece(4),
+              PuzzlePiece(5),
+              PuzzlePiece(6),
+              PuzzlePiece(7),
+              PuzzlePiece(8),
+            ],
+          );
+        }
+        return Container();
+      },
     );
   }
 }
 
 class PuzzlePiece extends StatefulWidget {
-  String imagePath;
-  double left;
-  double top;
-  int index;
-  _PuzzlePieceState _state;
+  int i;
 
-  Map get position {
-    return {'top': top, 'left': left};
-  }
-
-  String getImagePath() {
-    return _state.getImagePath();
-  }
-
-  void setImagePath(path) {
-    _state.setImagePath(path);
-  }
-
-  PuzzlePiece(index, imagePath, top, left) {
-    this.index = index;
-    this.imagePath = imagePath;
-    this.top = top;
-    this.left = left;
-  }
+  PuzzlePiece(this.i);
 
   @override
   _PuzzlePieceState createState() {
-    _state = _PuzzlePieceState(this.index, this.imagePath, this.top, this.left);
-    return _state;
+    double top, left;
+    if (i < 3) {
+      top = imageHeight + 30;
+    } else if (i < 6) {
+      top = imageHeight * 2 + inBetweenDistance + 30;
+    } else {
+      top = imageHeight * 3 + inBetweenDistance * 2 + 30;
+    }
+
+    if (i == 0 || i == 3 || i == 6) {
+      left = sideMargin;
+    } else if (i == 1 || i == 4 || i == 7) {
+      left = sideMargin + imageWidth + inBetweenDistance;
+    } else {
+      left = sideMargin + 2 * imageWidth + 2 * inBetweenDistance;
+    }
+    return _PuzzlePieceState(
+        i, images[i]['path'], top, left, images[i]['order']);
   }
 }
 
@@ -103,12 +109,14 @@ class _PuzzlePieceState extends State<PuzzlePiece> {
   double left;
   double top;
   int index;
+  int order;
 
-  _PuzzlePieceState(index, imagePath, top, left) {
+  _PuzzlePieceState(index, imagePath, top, left, order) {
     this.index = index;
     this.imagePath = imagePath;
     this.top = top;
     this.left = left;
+    this.order = order;
     pieceStates.add(this);
   }
 
@@ -116,9 +124,10 @@ class _PuzzlePieceState extends State<PuzzlePiece> {
     return imagePath;
   }
 
-  void setImagePath(path) {
+  void updateImage(path, order) {
     setState(() {
       imagePath = path;
+      this.order = order;
     });
   }
 
@@ -128,8 +137,13 @@ class _PuzzlePieceState extends State<PuzzlePiece> {
 
   @override
   Widget build(BuildContext context) {
+    Image image = Image.asset(
+      imagePath,
+      width: imageWidth,
+    );
+
     return Positioned(
-      width: pieceDimention,
+      width: imageWidth,
       top: top,
       left: left,
       child: Draggable(
@@ -138,216 +152,49 @@ class _PuzzlePieceState extends State<PuzzlePiece> {
             if (i == index) {
               continue;
             }
-            print((pieceStates[i].position['left'] - this.left).abs());
-            print(imagePath);
-            print(pieceStates[i].getImagePath());
             if ((pieceStates[i].position['left'] - details.offset.dx).abs() <
-                pieceDimention / 2 && (pieceStates[i].position['top'] - details.offset.dy).abs() <
-                pieceDimention / 2) {
+                    imageWidth / 2 &&
+                (pieceStates[i].position['top'] - details.offset.dy).abs() <
+                    imageWidth / 2) {
               String currentImagePath = imagePath;
-              setImagePath(pieceStates[i].getImagePath());
-              pieceStates[i].setImagePath(currentImagePath);
+              int currentOrder = order;
+              updateImage(pieceStates[i].getImagePath(), pieceStates[i].order);
+              pieceStates[i].updateImage(currentImagePath, currentOrder);
+              break;
             }
           }
+          if (puzzleCompleted()) {
+            print('DONE');
+          }
         },
-        onDragCompleted: () {},
-        child: Image.asset(
-          imagePath,
-        ),
-        feedback: Image.asset(
-          imagePath,
-          width: pieceDimention,
-        ),
+        child: image,
+        feedback: image,
         childWhenDragging: Container(),
       ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _HomePageState();
+bool puzzleCompleted() {
+  for (int i = 0; i < pieceStates.length; i++) {
+    if (i != pieceStates[i].order) {
+      return false;
+    }
   }
+  return true;
 }
 
-class _HomePageState extends State<HomePage> {
-  double width = 100.0, height = 100.0;
-  Offset position;
-
-  @override
-  void initState() {
-    super.initState();
-    position = Offset(0.0, height - 20);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
-    return Container(
-        color: Colors.black,
-        child: Column(children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              DragTarget(
-                builder: (
-                  BuildContext context,
-                  List incoming,
-                  List rejected,
-                ) {
-                  return Text('1');
-                },
-                onWillAccept: (data) {
-                  return true;
-                },
-              ),
-              Draggable(
-                data: 1,
-                child: Container(
-                  child: Image.asset('assets/images/1.jpg',
-                      width: width / 4.0, height: height / 4.0),
-                ),
-                feedback: Container(
-                  child: Image.asset('assets/images/1.jpg',
-                      width: width / 4.0, height: height / 4.0),
-                ),
-                childWhenDragging: Container(),
-              ),
-              DragTarget(
-                builder: (BuildContext context, incoming, rejected) {
-                  return Text('1');
-                },
-              ),
-              Draggable(
-                  data: 2,
-                  child: Container(
-                    child: Image.asset('assets/images/2.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(
-                    child: Image.asset('assets/images/2.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  childWhenDragging: Container()),
-              DragTarget(
-                builder: (BuildContext context, incoming, rejected) {
-                  return Text('1');
-                },
-              ),
-              Draggable(
-                  data: 3,
-                  child: Container(
-                    child: Image.asset('assets/images/3.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(
-                    child: Image.asset('assets/images/3.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  childWhenDragging: Container())
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Draggable(
-                  data: 4,
-                  child: Container(
-                    child: Image.asset('assets/images/4.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(
-                    child: Image.asset('assets/images/4.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  childWhenDragging: Container()),
-              Draggable(
-                  data: 5,
-                  child: Container(
-                    child: Image.asset('assets/images/5.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(
-                    child: Image.asset('assets/images/5.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  childWhenDragging: Container()),
-              Draggable(
-                  data: 6,
-                  child: Container(
-                    child: Image.asset('assets/images/6.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(
-                    child: Image.asset('assets/images/6.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  childWhenDragging: Container()),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Draggable(
-                  data: 7,
-                  child: Container(
-                    child: Image.asset('assets/images/7.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Container(),
-                  childWhenDragging: Container()),
-              Draggable(
-                data: 8,
-                child: Container(
-                  child: Image.asset('assets/images/8.jpg',
-                      width: width / 4.0, height: height / 4.0),
-                ),
-                feedback: Container(),
-                childWhenDragging: Container(),
-              ),
-              Draggable(
-                  data: 9,
-                  child: Container(
-                    child: Image.asset('assets/images/9.jpg',
-                        width: width / 4.0, height: height / 4.0),
-                  ),
-                  feedback: Image.asset('assets/images/9.jpg',
-                      width: width / 4.0, height: height / 4.0),
-                  childWhenDragging: Container()),
-            ],
-          )
-        ]));
-
-//   return   Stack(
-//      children: <Widget>[
-//        Positioned(
-//          left: position.dx,
-//          top: position.dy - height + 20,
-//          child: Draggable(
-//            child: Container(
-//              width: width,
-//              height: height,
-//              color: Colors.blue,
-//              child: Center(child: Text("Drag", style: Theme.of(context).textTheme.headline,),),
-//            ),
-//            feedback: Container(
-//              child: Center(
-//                child: Text("Drag", style: Theme.of(context).textTheme.headline,),),
-//              color: Colors.blue[300],
-//              width: width,
-//              height: height,
-//            ),
-//            onDraggableCanceled: (Velocity velocity, Offset offset){
-//              setState(() => position = offset);
-//            },
-//          ),
-//        ),
-//      ],
-//    );
-  }
+Future<Size> _calculateImageDimension() {
+  Completer<Size> completer = Completer();
+  Image image = Image.asset(images[0]['path']);
+  image.image.resolve(ImageConfiguration()).addListener(
+    ImageStreamListener(
+      (ImageInfo image, bool synchronousCall) {
+        var myImage = image.image;
+        Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
+        completer.complete(size);
+      },
+    ),
+  );
+  return completer.future;
 }
